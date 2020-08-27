f07c5dc5daab5a3bed5c2478586f80e7256a9c5e
ddb324 <jiangdaoran@163.com>
2020-08-28 00:30:51 +0800

    解决了svg自带fill属性的问题

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,16 +1,16 @@
 <template>
   <nav>
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="money"/>
       记账
     </router-link>
 
-    <router-link to="/labels" class="item">
-      <Icon name="label"/>
+    <router-link to="/labels" class="item" active-class="selected">
+      <Icon name="labels"/>
       标签
     </router-link>
 
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics"/>
       统计
     </router-link>
@@ -42,6 +42,9 @@ nav {
       width: 32px;
       height: 32px;
     }
+  }
+  >.item.selected{
+    color: red;
   }
 }
 </style>
