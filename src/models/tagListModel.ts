17aee957d9c94ddf8b86e73edd140d14b14d6281
ddb324 <jiangdaoran@163.com>
2020-09-07 00:28:51 +0800

    增加更新和删除的方法

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,3 +1,5 @@
+import createId from '@/lib/createId';
+
 const localStorageKeyName = 'tagList';
 type Tag = {
   id: string;
@@ -8,6 +10,8 @@ type TagListModel = {
   fetch: () => Tag[];
   save: () => void;
   create: (name: string | null) => 'success' | 'duplicated' | 'null';//success：成功，duplicated：name重复，null name为空
+  update: (id: string, name: string) => boolean;
+  remove: (id: string) => boolean;
 }
 const tagListModel: TagListModel = {
   data: [],
@@ -25,10 +29,39 @@ const tagListModel: TagListModel = {
     } else if (name === '' || name === null) {
       return 'null';
     } else {
-      this.data.push({id: name, name: name});
+      const id = createId().toString();
+      this.data.push({id, name});
       this.save();
       return 'success';
     }
+  },
+  update(id, name) {
+    const idList = this.data.map(item => item.id);
+    if (idList.indexOf(id) >= 0) {
+      const nameList = this.data.map(item => item.name);
+      if (nameList.indexOf(name) >= 0) {
+        return false;
+      } else {
+        const tag = this.data.filter(item => item.id === id)[0];
+        tag.name = name;
+        this.save();
+        return true;
+      }
+    } else {
+      return false;
+    }
+  },
+  remove(id) {
+    let index = -1;
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i].id === id) {
+        index = i;
+        break;
+      }
+    }
+    this.data.splice(index, 1);
+    this.save();
+    return true;
   }
 };
 export default tagListModel;
